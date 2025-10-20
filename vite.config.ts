7b87db32379a39e96d129bc956735c25f078f8ea
s241039 <s241039@kashiwanoha.ed.jp>
2025-10-20 13:48:20 +0900

    YY_251020_1
    サーバー動かせるようになったやつ

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    strictPort: true,
+    allowedHosts: ['4fbd4f82-9024-4bc2-af7f-fa3716fc9a6e-00-170d09lu9ey5a.sisko.replit.dev'],
+  }
 })
